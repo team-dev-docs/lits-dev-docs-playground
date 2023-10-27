9443382dc217588e31d562e1720cb5ccbf41e59f
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2023-10-27 20:00:53 +0000

    adds tutorial order

---
+++ javascript/ltsquickstart/chatengine.js
@@ -0,0 +1,37 @@
+import { stdin as input, stdout as output } from "node:process";
+// readline/promises is still experimental so not in @types/node yet
+// @ts-ignore
+import fs from "fs/promises";
+import readline from "node:readline/promises";
+
+import {
+  ContextChatEngine,
+  Document,
+  serviceContextFromDefaults,
+  VectorStoreIndex,
+} from "llamaindex";
+
+const essay = await fs.readFile(
+    "../text/essay.txt",
+    "utf-8",
+);
+
+async function main() {
+  const document = new Document({ text: essay });
+  const serviceContext = serviceContextFromDefaults({ chunkSize: 512 });
+  const index = await VectorStoreIndex.fromDocuments([document], {
+    serviceContext,
+  });
+  const retriever = index.asRetriever();
+  retriever.similarityTopK = 5;
+  const chatEngine = new ContextChatEngine({ retriever });
+  const rl = readline.createInterface({ input, output });
+
+  while (true) {
+    const query = await rl.question("Query: ");
+    const response = await chatEngine.chat(query);
+    console.log(response.toString());
+  }
+}
+
+main().catch(console.error);
